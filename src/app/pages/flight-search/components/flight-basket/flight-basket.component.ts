66734a9b339addd2a2dbf298c933ad73e0145f6e
Fabian Gerber <@work>
2018-10-30 15:22:08 +0100

    basket delete as ng-content

--- src/app/pages/flight-search/components/flight-basket/flight-basket.component.ts
+++ src/app/pages/flight-search/components/flight-basket/flight-basket.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-flight-basket',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightBasketComponent implements OnInit {
 
+  @Input() selectedFlightIds: boolean;
   constructor() { }
 
   ngOnInit() {
